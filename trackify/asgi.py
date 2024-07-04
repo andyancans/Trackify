3441b290e8c9094fa8545addea98eb613b099d29
Andy Ancans <andyancans1@gmail.com>
2024-07-04 09:02:39 +0100

    fixed directory / naming issues

--- trackify/asgi.py
+++ trackify/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Trackify project.
+ASGI config for trackify project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Trackify.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trackify.settings')
 
 application = get_asgi_application()
