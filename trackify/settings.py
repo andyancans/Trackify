a9f9cda001e1036a847d694b53a889fd0256381e
Andy <andyancans1@gmail.com>
2024-08-02 10:35:28 +0100

    added gunicorn and procfile

--- trackify/settings.py
+++ trackify/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-c&x5n3mta#)mn4!n1f#u2*gl=qm!2d=^$^w9adiut%h0h009ux
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', *.herokuapp.com,]
 
 
 # Application definition
